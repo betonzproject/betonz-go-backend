79c5a71c3d8a6ee4add06e56345b7043eb99a81b
doorman2137 <121001399+doorman2137@users.noreply.github.com>
2023-12-13 20:05:02 +0800

    refactor: Separate SQL queries into different files

---
+++ db/queries/transaction_request.sql
@@ -0,0 +1,31 @@
+-- name: GetTransactionRequests :many
+SELECT
+	tr.*,
+	u.username,
+	u.role,
+	u2.username AS "modifiedByUsername",
+	u2.role AS "modifiedByRole"
+FROM
+	"TransactionRequest" tr
+JOIN "User" u ON
+	u.id = tr."userId"
+LEFT JOIN "User" u2 ON
+	u2.id = tr."modifiedById"
+WHERE 
+	(@types::"TransactionType"[] IS NULL OR tr."type" = ANY(@types))
+AND
+	(@statuses::"TransactionStatus"[] IS NULL OR tr.status = ANY(@statuses))
+AND (
+	sqlc.narg('search')::text IS NULL
+	OR u.username ILIKE '%' || @search || '%' 
+	OR u2.username ILIKE '%' || @search || '%'
+	OR tr."bankAccountName" ILIKE '%' || @search || '%'
+	OR tr."beneficiaryBankAccountName" ILIKE '%' || @search || '%'
+	OR tr.remarks ILIKE '%' || @search || '%'
+)
+AND
+	tr."createdAt" >= sqlc.arg('fromDate')
+AND
+	tr."createdAt" < sqlc.arg('toDate')
+ORDER BY
+	tr.id DESC;
